85bc198a5e01d48ec63c90c3f96df8c6e7e0536c
JoshuaFaaui <joshuafaaui@gmail.com>
2023-01-05 22:37:27 +1300

    using mongodb inside conatiner

--- backend/index.js
+++ backend/index.js
@@ -5,39 +5,38 @@ const data = require('./structure')
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true)
 
-const details = 'mongodb+srv://Josh:HelloWorld@cluster0.refkjs4.mongodb.net/cluster0?retryWrites=true&w=majority'
+const details = "mongodb://mongo:27017/mongo-dockerised";
 mongoose.connect(details)
 .then(() => {
     console.log('connected to database')
+
+    app.get("/", (req, res) => {
+      data.find().then((results) => {
+        res.send(results);
+      });
+    });
+
+    app.post("/data", (req, res) => {
+      const Data = new data({
+        group: req.body.d1,
+        quote: req.body.d2,
+      });
+      Data.save();
+    });
+
+    app.get("/deleter", (req, res) => {
+      data
+        .deleteMany()
+        .then((result) => {
+          res.send(result);
+        })
+        .catch((err) => {
+          res.send(err);
+        });
+    });
+
+    app.listen(3001);
 })
 .catch((err)=>{console.log(err)})
-
-app.get('/', (req,res) => {
-    data.find()
-        .then((results) => {
-        res.send(results)
-    })
-})
-
-app.post("/data", (req, res) => {
-    const Data = new data({
-        group: req.body.d1,
-        quote: req.body.d2
-    })
-    Data.save()
-});
-
-app.get('/deleter', (req, res) => {
-    data
-      .deleteMany()
-      .then((result) => {
-        res.send(result);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-})
-
-app.listen(3001)
